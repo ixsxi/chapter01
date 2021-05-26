0ce390f5c1f7beda9d5332d2008303f701907553
user <user@DESKTOP-PMC72TK>
2021-05-26 23:21:22 +0900

    5/26 챕터 1

---
+++ src/com/javaex/ex03/Ex04_1.java
@@ -0,0 +1,28 @@
+package com.javaex.ex03;
+
+import java.util.Scanner;
+
+public class Ex04_1 {
+
+	public static void main(String[] args) {
+		int sum;
+		// 조건문 연습문제
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("근무시간");
+
+		int time = sc.nextInt();
+
+		if (time > 8) {
+			sum = 8 * 10000 + (time - 8) * 12000;
+			System.out.println(sum);
+		} else {
+			sum = time * 10000;
+			System.out.println(sum);
+		}
+
+		sc.close();
+
+	}
+
+}
